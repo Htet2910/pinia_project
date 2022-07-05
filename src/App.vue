3e7a2e568d786d6c91dcc9da8cea56114628ef16
HHEZ <you@example.com>
2022-07-05 13:42:30 +0630

    study pinia

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,38 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <h3>Hello World {{name}}</h3>
+   <h4>{{counter}}</h4>
+   <h4>{{doubleCount}}</h4>
+  <button @click="add(15)">Click Me</button>
+  <button @click="clear">Reset Store</button>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import { useCounterStore } from '@/store/useCounter';
+import {  storeToRefs } from 'pinia';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    const main = useCounterStore();
+    const {counter,name,doubleCount} =storeToRefs(main);
+    
+    //  const {addOne} = main;
+    // const {addOne} = mapActions(useCounterStore,["addOne"]);
+
+    function add(value){
+      //  main.$patch({
+      //   counter:counter.value + value
+      //  });
+      main.$patch(state => (state.counter+=value));
+    }
+    // function reset(){
+    //   main.$reset();
+    // }
+    function clear(){
+      main.$state={counter:123,name:'bob'};
+    }
+  main.$subscribe((mutation,state) =>{
+    console.log("mutation",mutation);
+    console.log("state",state);
+
+  });
 </script>
 
 <style>
